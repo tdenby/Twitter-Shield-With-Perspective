1940089a0cdc5f6ce4c31477f67bd3714f1d4829
tdenby <tdenby@umich.edu>
2019-08-12 16:13:56 -0400

    added logging out feature

--- Twitter-Chrome-Mod-Version-2/js/popup.js
+++ Twitter-Chrome-Mod-Version-2/js/popup.js
@@ -1,3 +1,4 @@
+
 console.log("popup.js");
 // This extension loads the saved background color for the current tab if one
 // exists. The user can select a new background color from the dropdown for the
@@ -8,7 +9,35 @@ console.log("popup.js");
 // chrome.storage.local allows the extension data to be synced across multiple
 // user devices.
 
+function skipLogin() {
+  if(localStorage.getItem('username') != null) {
+    document.getElementById('statePanel').innerHTML = 'Logged in as <b>@'  + localStorage.getItem('username') + '</b>'
 
+
+ 
+  
+  document.getElementById('submitPanel').remove();
+
+   var learnMore = document.createElement('div')
+  learnMore.innerHTML = 'Learn how Stranger Danger works'
+  learnMore.style = 'padding: 2px 10px; text-align: center; border-radius: 8px; background-color: #67c4e7; '
+                  + 'text-decoration: none; display: font-size: 14px; '
+                  + 'margin-left:15px; margin-right:15px; margin-top:3px; margin-bottom: 3px; cursor: pointer; color:white;'
+
+  var btn = document.createElement('div')
+  btn.innerHTML = 'Log in with a different handle'
+  btn.style = 'padding: 2px 10px; text-align: center; border-radius: 8px; background-color: #428bca; '
+                  + 'text-decoration: none; display: font-size: 14px; '
+                  + 'margin-left:15px; margin-right:15px; margin-top:3px; margin-bottom: 3px; cursor: pointer; color:white;'
+  
+  document.getElementById('allPanel').append(learnMore)
+  document.getElementById('allPanel').append(btn)
+
+  document.getElementById('statePanel').innerHTML = 'Logged in as <b>@'  + localStorage.getItem('username') + '</b>'
+  }
+}
+
+//document.getElementById('statePanel').innerHTML = 'Logged in as <b>@'  + chrome.storage.local.get(['username']) + '</b>'
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('event listener for popup')
@@ -32,11 +61,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //   // });
 
-});
+}); 
 
 function setAccountHandle(){
 
-  chrome.tabs.executeScript({
+  document.addEventListener('DOMContentLoaded', function () {
+  console.log('event listener for popup')
+  document.getElementById('submitId').addEventListener('click', setAccountHandle);
+  console.log('set user id')
+  // document.getElementById('submitId').addEventListener('click', setThreshold);
+  
+  
+//   // console.log(msg);
+//   // chrome.tabs.query({
+//   //   active: true,
+//   //   currentWindow: true
+//   // }, function(tabs) {
+//   //   chrome.tabs.sendMessage(tabs[0].id, {
+//   //     greeting: 'hello'
+//   //   }, function(response) {
+//   //     console.log(response.data);
+//   //     var pageUrl = (tabs[0].url);
+//   //     msg.innerText = response.data;
+//   //   });
+
+//   // });
+
+}); 
+/*localStorage.setItem({username: document.getElementById('accountHandle').value}, function() {
+  console.log('Value is set to ' + document.getElementById('accountHandle').value);
+});*/
+    chrome.tabs.executeScript({
         code: "var accountName =" + '"' + document.getElementById('accountHandle').value +'";',
         allFrames: true
     }, function(result) {
@@ -44,62 +99,67 @@ function setAccountHandle(){
          
         });
     });
-
-<<<<<<< HEAD
-var slider = document.getElementById("thresholdId");
-var output = document.getElementById("demo");
-output.innerHTML = 'Low Toxicity'; // Display the default slider value
-   
-slider.oninput = function() {
-  if(slider.value < .2) {
-    output.innerHTML = 'Low Toxicity';
-  }
-  else if((slider.value >= .2) && (slider.value < .4)) {
-    output.innerHTML = 'Moderate Toxicity';
-  }
-  else if((slider.value >= .4) && (slider.value < .6)) {
-    output.innerHTML = 'Considerable Toxicity';
-  }
-  else if((slider.value >= .6) && (slider.value < .8)) {
-    output.innerHTML = 'High Toxicity';
-  }
-  else if((slider.value >= .8) && (slider.value <= 1)) {
-    output.innerHTML = 'Severe Toxicity';
-  }
-  //output.innerHTML = this.value;
-}
-
-// maybe 
-// chrome.runtime.onInstalled.addListener(function callback)
-// chrome.runtime.onInstalled.addListener(function object(){
-=======
-  document.getElementById('statePanel').innerHTML = 'Logged in as <b>@'  + document.getElementById('accountHandle').value + '</b>'
+    localStorage.setItem('username', document.getElementById('accountHandle').value);
+  document.getElementById('statePanel').innerHTML = 'Logged in as <b>@'  + localStorage.getItem('username') + '</b>'
 
 
  
   
   document.getElementById('submitPanel').remove();
 
-   var learnMore = document.createElement('div')
+  var learnMore = document.createElement('div')
   learnMore.innerHTML = 'Learn how Stranger Danger works'
   learnMore.style = 'padding: 2px 10px; text-align: center; border-radius: 8px; background-color: #67c4e7; '
                   + 'text-decoration: none; display: font-size: 14px; '
                   + 'margin-left:15px; margin-right:15px; margin-top:3px; margin-bottom: 3px; cursor: pointer; color:white;'
 
-  var logBack = document.createElement('div')
-  logBack.innerHTML = 'Log in with a different handle'
-  logBack.style = 'padding: 2px 10px; text-align: center; border-radius: 8px; background-color: #428bca; '
+  var btn = document.createElement('div')
+  btn.innerHTML = 'Log in with a different handle'
+  btn.style = 'padding: 2px 10px; text-align: center; border-radius: 8px; background-color: #428bca; '
                   + 'text-decoration: none; display: font-size: 14px; '
                   + 'margin-left:15px; margin-right:15px; margin-top:3px; margin-bottom: 3px; cursor: pointer; color:white;'
   
+  var slider = document.createElement('div')
+  slider.className = 'slidecontainer';
+  document.getElementById('allPanel').append(slider)
   document.getElementById('allPanel').append(learnMore)
-  document.getElementById('allPanel').append(logBack)
+  document.getElementById('allPanel').append(btn)
 
-  document.getElementById('statePanel').innerHTML = 'Logged in as <b>@'  + document.getElementById('accountHandle').value + '</b>'
+
+  btn.addEventListener('click', logOut, false);
+  document.getElementById('statePanel').innerHTML = 'Logged in as <b>@'  + localStorage.getItem(['username']) + '</b>'
                                                               + '<br> You successfully logged in..'
->>>>>>> 0403ba07a65e2ee7f0aa6078c4c8514e1e0da852
+                                                        
 
 
+}
+
+function logOut() {
+  localStorage.removeItem('username');
+  document.location.reload()
+}
+
+var slider = document.getElementById("thresholdId");
+var output = document.getElementById("demo");
+output.innerHTML = 'Less than 20% Toxic Tweets'; // Display the default slider value
+   
+slider.oninput = function() {
+  if(slider.value < .2) {
+    output.innerHTML = 'Less than 20% Toxic Tweets';
+  }
+  else if((slider.value >= .2) && (slider.value < .4)) {
+    output.innerHTML = 'Less than 40% Toxic Tweets';
+  }
+  else if((slider.value >= .4) && (slider.value < .6)) {
+    output.innerHTML = 'Less than 60% Toxic Tweets';
+  }
+  else if((slider.value >= .6) && (slider.value < .8)) {
+    output.innerHTML = 'Less than 80% Toxic Tweets';
+  }
+  else if((slider.value >= .8) && (slider.value <= 1)) {
+    output.innerHTML = 'Less than 100% Toxic Tweets';
+  }
+  //output.innerHTML = this.value;
 }
 
 
